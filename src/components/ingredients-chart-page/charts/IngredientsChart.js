a96b1c87559a04a04daad1e8b63764201408e2e1
Cristina Coppola <crs.coppola@gmail.com>
2020-07-08 22:56:01 +0100

    Primi esperimenti con le opzioni di Vis

--- src/components/ingredients-chart-page/charts/IngredientsChart.js
+++ src/components/ingredients-chart-page/charts/IngredientsChart.js
@@ -6,6 +6,28 @@ import Typography from "@material-ui/core/Typography";
 import YouTube from 'react-youtube';
 import * as _ from 'lodash';
 
+const options = {
+    autoResize: true,
+    groups: {
+        myGroup: {color: "#e04141", borderWidth: 3}
+    },
+    layout: {
+        hierarchical: false
+    },
+    physics: {
+        solver: 'forceAtlas2Based',
+        forceAtlas2Based: {
+            damping: 0.1
+        }
+    },
+    interaction: {
+        multiselect: true
+    },
+    edges: {
+        color: "#000000"
+    }
+};
+
 class IngredientsChart extends Component {
     clicks = 0
 
@@ -26,6 +48,7 @@ class IngredientsChart extends Component {
             <div>
                 <VisNetworkReactComponent
                     data={this.state.graph}
+                    options={options}
                     events={this.events}
                     getNetwork={this.getNetwork}
                     style={{height: "100vh"}}
