86f4db00bc85e5d24ad216f7ade4505c0b47868e
Cristina Coppola <crs.coppola@gmail.com>
2020-07-11 16:02:41 +0100

    Parametrizzato, adesso il Drawer accetta il contenuto come parametro

--- src/components/drawer/Drawer.js
+++ src/components/drawer/Drawer.js
@@ -3,16 +3,35 @@ import clsx from 'clsx';
 import {makeStyles} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 
+const useStyles = makeStyles({
+    list: {
+        width: 500,
+    },
+    fullList: {
+        width: 'auto',
+    },
+});
+
 export default function TemporaryDrawer(props) {
+    const classes = useStyles();
+
     const {open, content, onClose} = props;
 
     return (
         <React.Fragment key={"left"}>
             <Drawer
                 anchor={"left"}
-                    open={open}
-                    onClose={onClose}
+                open={open}
+                onClose={onClose}
             >
+                <div
+                    className={clsx(classes.list, {
+                        [classes.fullList]: false,
+                    })}
+                    role="presentation"
+                >
+                    {content()}
+                </div>
             </Drawer>
         </React.Fragment>
     );
